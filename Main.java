1271e2b994e56f0f866062687004978cfe9739c5
siroratt suntronsuk <soulyu_shine@hotmail.com>
2017-05-06 11:24:00 +0700

    add printout Hello3

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] argv) {
         System.out.println("Hello world 1");
         System.out.println("Hello world 2");
+        System.out.println("Hello world 3");
     }
 }
