001a1f9642d7654b46730ee5765ed7f3dbed5cfe
siroratt suntronsuk <soulyu_shine@hotmail.com>
2017-05-06 11:41:39 +0700

    commit spilt line

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 class Main {
     public static void main(String[] argv) {
+        System.out.println("My name is Nong");
+        System.out.println("My name is Nong");
         System.out.println("Hello world 1");
+        System.out.println("My name is Tom");
+        System.out.println("My name is Nong");
         System.out.println("Hello world 2");
+        System.out.println("My name is Auu");
+        System.out.println("My name is Nong");
         System.out.println("Hello world 3");
     }
 }
